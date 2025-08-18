7b553573bfb198f31f4f7d3f48ce65ac2e7fae95
RainCreatorHub <rain.hub.creator@gmail.com>
2025-08-18 14:37:32 -0300

    Update Games.Supported.lua

--- Games/Games.Supported.lua
+++ Games/Games.Supported.lua
@@ -1,1 +1,8 @@
-
+local Button = Games:Button({
+    Title = "[🐟] 99 noites na floresta",
+    Desc = "Clique para ir para o Lobby",
+    Locked = false,
+    Callback = function()
+        print("clicked")
+    end
+})
